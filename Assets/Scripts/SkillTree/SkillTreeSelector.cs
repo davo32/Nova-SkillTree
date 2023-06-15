218a35bde3129c531d57f381c0ab98d317bba4f7
davo32 <davo4200@gmail.com>
2023-06-16 00:29:06 +1000

    Added Identifier for the different skill trees

--- Assets/Scripts/SkillTree/SkillTreeSelector.cs
+++ Assets/Scripts/SkillTree/SkillTreeSelector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,7 @@ namespace NovaSkillTree
     public class SkillTreeSelector : MonoBehaviour
     {
         public GameObject MainScreen;
+        public GameObject SkillTreeID;
         public GameObject[] SkillTreeScreens;
 
 
@@ -20,6 +22,8 @@ namespace NovaSkillTree
             MainScreen.SetActive(false);
             SkillTreeScreens[i].gameObject.SetActive(true);
             scrollView.GetComponent<ScrollRect>().content = SkillTreeScreens[i].GetComponent<RectTransform>();
+            SkillTreeID.SetActive(true);
+            SkillTreeID.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = SkillTreeScreens[i].name;
             BackBtn.interactable = true;
         }
 
@@ -30,6 +34,8 @@ namespace NovaSkillTree
                 SkillTreeScreens[i].gameObject.SetActive(false);
             }
 
+            SkillTreeID.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = " ";
+            SkillTreeID.SetActive(false);
             BackBtn.interactable = false;
             MainScreen.SetActive(true);
             scrollView.GetComponent<ScrollRect>().content = MainScreen.GetComponent<RectTransform>();
